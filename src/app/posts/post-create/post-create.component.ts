b070898c485c6aa5d9a7ac7ee3a8d1f8dce2a424
nabil bg <nabil.bg7@gmail.com>
2018-10-15 20:53:27 +0300

    Adding Post Model

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core'
 
+import { Post } from '../post.model'
 @Component({
   selector: "app-post-create",
   templateUrl: "./post-create.component.html",
@@ -9,9 +10,9 @@ export class PostCreateComponent{
   enteredTitle='';
   enteredBody=""
   //Listen to the event from user input
-  @Output() postCreated = new EventEmitter();
+  @Output() postCreated = new EventEmitter<Post>();
   onAddPost(){
-    const post = {
+    const post: Post = {
       title: this.enteredTitle,
       body: this.enteredBody
     }
